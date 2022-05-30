27e349af68409689b0233e4609fa0fc90b3fd3b1
wldms2494 <wldms2494@gmail.com>
2022-05-30 15:54:32 +0900

    caseList 필드 추가 , boxes에 테스트용 버튼 추가 onClick

--- force-app/main/default/lwc/boxes/boxes.js
+++ force-app/main/default/lwc/boxes/boxes.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
 
-export default class Boxes extends LightningElement {}
+export default class Boxes extends LightningElement {
+
+
+    greeting = "hello";
+    name = {first: "john", last:" Doe"};
+    handleClick() {
+        this.greeting = "goodbye";
+        this.name.first="Ollie";
+    }
+}
--- force-app/main/default/lwc/caseList/caseList.js
+++ force-app/main/default/lwc/caseList/caseList.js
@@ -3,12 +3,16 @@ import getCases from '@salesforce/apex/CaseController.getCases';
 import SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
 import NUMBER_FIELD from '@salesforce/schema/Case.CaseNumber';
 import ACCOUNT_FIELD from '@salesforce/schema/Case.AccountId';
+import REASON_FIELD from '@salesforce/schema/Case.Reason';
+import STATUS_FIELD from '@salesforce/schema/Case.Status';
 
 
 const COLUMNS = [
     { label: 'Case Number', fieldName: NUMBER_FIELD.fieldApiName , type:'text'},
     { label: 'Account Name', fieldName: ACCOUNT_FIELD.fieldApiName , type:'text'},
-    { label: 'Subject', fieldName: SUBJECT_FIELD.fieldApiName , type:'text'}
+    { label: 'Status', fieldName: STATUS_FIELD.fieldApiName , type:'text'},
+    { label: 'Subject', fieldName: SUBJECT_FIELD.fieldApiName , type:'text'},
+    { label: 'Reason', fieldName: REASON_FIELD.fieldApiName , type:'text'},
     ,
 
 
