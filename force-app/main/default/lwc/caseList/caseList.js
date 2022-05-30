cef1429489263f67598220aecc0030d0eaccd6cf
wldms2494 <wldms2494@gmail.com>
2022-05-30 11:09:45 +0900

    caseList 에 필드 추가

--- force-app/main/default/lwc/caseList/caseList.js
+++ force-app/main/default/lwc/caseList/caseList.js
@@ -1,10 +1,16 @@
 import { LightningElement, wire } from 'lwc';
 import getCases from '@salesforce/apex/CaseController.getCases';
-import SUBJECT_FIELD from '@salesforce/schema/Case.Subject'
+import SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
+import NUMBER_FIELD from '@salesforce/schema/Case.CaseNumber';
+import ACCOUNT_FIELD from '@salesforce/schema/Case.AccountId';
+
 
 const COLUMNS = [
-   
-    { label: 'Subject', fieldName: SUBJECT_FIELD.fieldApiName , type:'text'},
+    { label: 'Case Number', fieldName: NUMBER_FIELD.fieldApiName , type:'text'},
+    { label: 'Account Name', fieldName: ACCOUNT_FIELD.fieldApiName , type:'text'},
+    { label: 'Subject', fieldName: SUBJECT_FIELD.fieldApiName , type:'text'}
+    ,
+
 
 
 ];
