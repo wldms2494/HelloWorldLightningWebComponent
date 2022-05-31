343acf8832677af7c1c1e7dfaa05f4c1cfee7dc5
wldms2494 <wldms2494@gmail.com>
2022-05-31 13:46:19 +0900

    데이터테이블에 action버튼에 delete 생김, 기능은 실행 안됨

--- force-app/main/default/lwc/boxes/boxes.js
+++ force-app/main/default/lwc/boxes/boxes.js
@@ -10,7 +10,7 @@ export default class Boxes extends  LightningElement {
     @wire(getNewRecordCounts) newCount;
     @wire(getPendingRecordCounts) workingCount;
     @wire(getClosedRecordCounts) closedCount;
-
+      
 }
 
 
--- force-app/main/default/lwc/caseList/caseList.js
+++ force-app/main/default/lwc/caseList/caseList.js
@@ -1,11 +1,17 @@
 import { LightningElement, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 import getCases from '@salesforce/apex/CaseController.getCases';
 import SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
 import NUMBER_FIELD from '@salesforce/schema/Case.CaseNumber';
 import ACCOUNT_FIELD from '@salesforce/schema/Case.AccountId';
 import REASON_FIELD from '@salesforce/schema/Case.Reason';
 import STATUS_FIELD from '@salesforce/schema/Case.Status';
+import deleteRow from '@salesforce/apex/RowActionsHandler.deleteRow';
 
+//define row actions
+const actions = [
+    {label:'Delete', name:'delete'}
+];
 
 const COLUMNS = [
     { label: 'Case Number', fieldName: NUMBER_FIELD.fieldApiName , type:'text'},
@@ -13,15 +19,26 @@ const COLUMNS = [
     { label: 'Status', fieldName: STATUS_FIELD.fieldApiName , type:'text'},
     { label: 'Subject', fieldName: SUBJECT_FIELD.fieldApiName , type:'text'},
     { label: 'Reason', fieldName: REASON_FIELD.fieldApiName , type:'text'},
+    {
+        type: 'action',
+        typeAttributes:{
+            rowActions: actions,
+            menuAlignment: 'right'
+        }
+    }
     ,
 
 
 
 ];
 
-export default class CaseList extends LightningElement {
+export default class CaseList extends NavigationMixin(LightningElement) {
 
     columns = COLUMNS;
     @wire(getCases)
     cases;
+    refreshTable;
+
+
+   
 }
