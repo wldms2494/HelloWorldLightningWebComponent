5b1485809f41bac12ee0a47ab95d2546f142460b
wldms2494 <wldms2494@gmail.com>
2022-06-02 17:26:29 +0900

    boxes 랑 caseList 를 voc 하나로 합침

---
+++ force-app/main/default/lwc/voc/voc.js
@@ -0,0 +1,39 @@
+import { LightningElement, wire } from 'lwc';
+import getRecordCounts from '@salesforce/apex/recordCount.getRecordCounts';
+import getClosedRecordCounts from '@salesforce/apex/recordCount.getClosedRecordCounts';
+import getPendingRecordCounts from '@salesforce/apex/recordCount.getPendingRecordCounts';
+import getNewRecordCounts from '@salesforce/apex/recordCount.getNewRecordCounts';
+
+import getAllCases from '@salesforce/apex/CaseController.getAllCases';
+
+import SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
+import NUMBER_FIELD from '@salesforce/schema/Case.CaseNumber';
+import ACCOUNT_FIELD from '@salesforce/schema/Case.AccountId';
+import REASON_FIELD from '@salesforce/schema/Case.Reason';
+import STATUS_FIELD from '@salesforce/schema/Case.Status';
+
+
+const COLUMNS = [
+    { label: 'Case Number', fieldName: NUMBER_FIELD.fieldApiName , type:'text'},
+    { label: 'Account Name', fieldName: ACCOUNT_FIELD.fieldApiName , type:'text'},
+    { label: 'Status', fieldName: STATUS_FIELD.fieldApiName , type:'text'},
+    { label: 'Subject', fieldName: SUBJECT_FIELD.fieldApiName , type:'text'},
+    { label: 'Reason', fieldName: REASON_FIELD.fieldApiName , type:'text'},
+    
+];
+
+
+
+
+export default class Voc extends LightningElement {
+
+    columns = COLUMNS;
+    @wire(getRecordCounts) count;
+    @wire(getNewRecordCounts) newCount;
+    @wire(getPendingRecordCounts) workingCount;
+    @wire(getClosedRecordCounts) closedCount;
+
+    @wire(getAllCases) allCases;
+
+
+}
